0165822a622a6141918ca83ee3eee5b0d3380083
Christian Diamantis <diamantischristian@gmail.com>
2020-04-02 18:25:41 +0300

    Removed Google Services, Updated GitIgnore

--- lib/classes/background_gradient.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class BackgroundGradient extends BoxDecoration {
-  @override
-  Gradient get gradient => LinearGradient(
-      begin: Alignment.topCenter,
-      end: Alignment.bottomCenter,
-      colors: [Colors.green.shade50, Colors.green]);
-}
